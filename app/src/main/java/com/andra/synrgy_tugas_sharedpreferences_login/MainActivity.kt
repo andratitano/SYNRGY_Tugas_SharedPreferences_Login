73aeb9642c3344b35c3b7f539e6d3f3c0d3fd206
andratitano <andrabusra10@gmail.com>
2020-08-10 11:52:07 +0700

    menambahkan log out

--- app/src/main/java/com/andra/synrgy_tugas_sharedpreferences_login/MainActivity.kt
+++ app/src/main/java/com/andra/synrgy_tugas_sharedpreferences_login/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.andra.synrgy_tugas_sharedpreferences_login
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.tvUsername
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +23,16 @@ class MainActivity : AppCompatActivity() {
         val userNameInSP = sharedPreferences.getString(UserData.FIELD_USERNAME, "Guest")
 
         tvUsername.setText(userNameInSP)
+
+        btnClear.setOnClickListener {
+            val editor = sharedPreferences.edit()
+            editor.clear()
+            editor.apply()
+            tvUsername.setText("")
+            Toast.makeText(this@MainActivity, "Data Clear", Toast.LENGTH_SHORT).show()
+
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
